8ced3fd3f056536d7e4c9cecd650c3056e02dfc7
Leandro Silva <leandro.silva@zooxsmart.com>
2021-05-03 13:52:57 -0300

    Update codebase and add php 8 support

--- src/ResponseTime.php
+++ src/ResponseTime.php
@@ -1,4 +1,7 @@
 <?php
+
+declare(strict_types=1);
+
 namespace LosMiddleware\ResponseTime;
 
 use Psr\Http\Message\ResponseInterface;
@@ -6,45 +9,39 @@ use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 
+use function array_key_exists;
+use function microtime;
+use function sprintf;
+
 final class ResponseTime implements MiddlewareInterface
 {
+    public const HEADER_NAME = 'X-Response-Time';
 
-    const HEADER_NAME = 'X-Response-Time';
+    /** @var array{header_name: string} */
+    private array $options;
 
-    /**
-     *
-     * @var array
-     */
-    private $options;
-
-    /**
-     * ResponseTime constructor.
-     * @param array $options
-     */
-    public function __construct($options = [])
+    /** @param array{header_name: string} $options */
+    public function __construct(array $options = [])
     {
-        $this->options = array_merge([
-            'header_name' => self::HEADER_NAME
-        ], $options);
+        $this->options = [
+            'header_name' => $options['header_name'] ?? self::HEADER_NAME,
+        ];
     }
 
-    /**
-     * @param ServerRequestInterface $request
-     * @param RequestHandlerInterface $handler
-     * @return ResponseInterface
-     */
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
         $server = $request->getServerParams();
 
         if (! array_key_exists('REQUEST_TIME_FLOAT', $server)) {
-            $server['REQUEST_TIME_FLOAT'] = microtime(true);
+            $requestTime = microtime(true);
+        } else {
+            $requestTime = (float) $server['REQUEST_TIME_FLOAT'];
         }
 
         $response = $handler->handle($request);
 
-        $time = (microtime(true) - $server['REQUEST_TIME_FLOAT']) * 1000;
+        $time = (microtime(true) - $requestTime) * 1000;
 
-        return $response->withHeader(self::HEADER_NAME, sprintf('%2.2fms', $time));
+        return $response->withHeader($this->options['header_name'], sprintf('%2.2fms', $time));
     }
 }
